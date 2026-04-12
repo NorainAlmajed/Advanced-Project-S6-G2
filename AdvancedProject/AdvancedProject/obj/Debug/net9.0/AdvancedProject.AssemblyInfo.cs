0d4f8742a5dfc53c95408499982d2a300a98ba31
Zahraa Hubail <202305220@student.polytechnic.bh>
2026-04-12 19:54:19 +0300

    Added SeedData

--- AdvancedProject/AdvancedProject/obj/Debug/net9.0/AdvancedProject.AssemblyInfo.cs
+++ AdvancedProject/AdvancedProject/obj/Debug/net9.0/AdvancedProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdvancedProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c700910e4986ca557e6eb1acaaa0f6ba0e9d5430")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+499140d0ae534a6b8937a812dccc832e15f9f93a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdvancedProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdvancedProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
