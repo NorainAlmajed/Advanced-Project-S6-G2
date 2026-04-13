fc2e771157abefef4be6cbbba717a3c24029ab23
Zahraa Hubail <202305220@student.polytechnic.bh>
2026-04-13 17:47:45 +0300

    Added Initial Create Migration

--- AdvancedProject/AdvancedProject/obj/Debug/net9.0/AdvancedProject.AssemblyInfo.cs
+++ AdvancedProject/AdvancedProject/obj/Debug/net9.0/AdvancedProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdvancedProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b3c451c38a448254f961ba6e1b50f23bdcfd3ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80fd94f6f534ced5bdc40c63645dadfc35cbe11e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdvancedProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdvancedProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
