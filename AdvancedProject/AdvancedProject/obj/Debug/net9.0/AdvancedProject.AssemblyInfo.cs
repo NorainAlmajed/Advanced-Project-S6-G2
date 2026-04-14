87417817d9d8158e65fd5cd8c81e348bb6fa9b76
Zahraa Hubail <202305220@student.polytechnic.bh>
2026-04-14 20:26:10 +0300

    Fixed Lease Application Form Error

--- AdvancedProject/AdvancedProject/obj/Debug/net9.0/AdvancedProject.AssemblyInfo.cs
+++ AdvancedProject/AdvancedProject/obj/Debug/net9.0/AdvancedProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdvancedProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62785a9eb67d1b1f1be14f5a2c943a8a85adbd90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+012c7738634ab48356e4a08061a3f402d3e6709f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdvancedProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdvancedProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
