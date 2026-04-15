a5df0f538253d964ee7853a75cc996113a315a53
RaghadAlesakfi <202302130@student.polytechnic.bh>
2026-04-15 23:11:54 +0300

    fixed the search bar in the maintincesRequsest

--- AdvancedProject/AdvancedProject/obj/Debug/net9.0/AdvancedProject.AssemblyInfo.cs
+++ AdvancedProject/AdvancedProject/obj/Debug/net9.0/AdvancedProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdvancedProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e9891d5c9b22e8c8c612c601af8b25584aa3280")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c88d92b4e7a4095fc503c77ca59614c5bc45c1de")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdvancedProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdvancedProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
