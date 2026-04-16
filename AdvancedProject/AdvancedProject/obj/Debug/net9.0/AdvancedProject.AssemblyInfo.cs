41119e1bd1f5299525f85d0695cc326e79840e38
RaghadAlesakfi <202302130@student.polytechnic.bh>
2026-04-16 23:57:28 +0300

    added search in the dropdown of the create lease form

--- AdvancedProject/AdvancedProject/obj/Debug/net9.0/AdvancedProject.AssemblyInfo.cs
+++ AdvancedProject/AdvancedProject/obj/Debug/net9.0/AdvancedProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdvancedProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28eee87b127f6098fb8bb5a318a64ad7bab9c038")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+870a5f67a634f46d3de19dce5ff039b84e9edb81")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdvancedProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdvancedProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
