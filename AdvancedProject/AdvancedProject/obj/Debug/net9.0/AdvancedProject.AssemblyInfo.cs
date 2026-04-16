d7ad7ffcfed134762ebc453aa0750f90c35f8a3c
Zahraa Hubail <202305220@student.polytechnic.bh>
2026-04-16 22:24:15 +0300

    Added Edit Lease Form

--- AdvancedProject/AdvancedProject/obj/Debug/net9.0/AdvancedProject.AssemblyInfo.cs
+++ AdvancedProject/AdvancedProject/obj/Debug/net9.0/AdvancedProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdvancedProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6f389e6e4a99883cb2de0dabff09ca2e1063e68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfee915b1b0b5ac91009ab344d304beef004b527")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdvancedProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdvancedProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
