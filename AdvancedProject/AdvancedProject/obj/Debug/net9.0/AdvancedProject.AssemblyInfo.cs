b08ab60c8f41308ed7c16761fbe0eb172a8484e2
Zahraa Hubail <202305220@student.polytechnic.bh>
2026-04-18 01:49:55 +0300

    changes

--- AdvancedProject/AdvancedProject/obj/Debug/net9.0/AdvancedProject.AssemblyInfo.cs
+++ AdvancedProject/AdvancedProject/obj/Debug/net9.0/AdvancedProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdvancedProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16321fd646f21e5e3836b123fb8bfbaf9d8b0b94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13f20da8b4301ef7ed569b0cb52a9f50b49ae5a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdvancedProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdvancedProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
