07be3d08e84e5dd337cce2ac38308cc989a6c0eb
RaghadAlesakfi <202302130@student.polytechnic.bh>
2026-04-18 00:50:24 +0300

    change the radius of the users tenets maintenance buttons in the users page

--- AdvancedProject/AdvancedProject/obj/Debug/net9.0/AdvancedProject.AssemblyInfo.cs
+++ AdvancedProject/AdvancedProject/obj/Debug/net9.0/AdvancedProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdvancedProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8816ecb52011065c41ceee7612c79bc18cb8da7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4df6af8ef2db0e190cda35c02bf02e04dbb04a0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdvancedProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdvancedProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
