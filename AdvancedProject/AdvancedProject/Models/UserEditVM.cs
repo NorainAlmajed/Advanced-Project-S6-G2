32029559333178004bbab9b334f21938f219ed69
Zahraa Hubail <202305220@student.polytechnic.bh>
2026-04-18 03:23:28 +0300

    Added Edit Manager Form

---
+++ AdvancedProject/AdvancedProject/Models/UserEditVM.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AdvancedProject.Models
+{
+    public class UserEditVM
+    {
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = "Username is required")]
+        public string Username { get; set; }
+
+        public string? Password { get; set; } // optional ✔
+
+        [Required(ErrorMessage = "Full name is required")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone is required")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Gender is required")]
+        public string Gender { get; set; }
+    }
+}
--- AdvancedProject/AdvancedProject/obj/Debug/net9.0/AdvancedProject.AssemblyInfo.cs
+++ AdvancedProject/AdvancedProject/obj/Debug/net9.0/AdvancedProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdvancedProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14215831cb6d85ae4b09011a04625975951565f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c64d4acad760b8f81928b00b409535b7943ad64a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdvancedProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdvancedProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
