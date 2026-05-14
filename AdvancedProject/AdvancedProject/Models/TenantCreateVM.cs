b11b88d64cc596f8aa7d099c3ec07b14c64adcb3
Zahraa Hubail <202305220@student.polytechnic.bh>
2026-05-14 20:12:14 +0300

    Added confirm password field

--- AdvancedProject/AdvancedProject/Models/TenantCreateVM.cs
+++ AdvancedProject/AdvancedProject/Models/TenantCreateVM.cs
@@ -1,15 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
-
 namespace AdvancedProject.Models
 {
     public class TenantCreateVM
     {
-        // User fields
         [Required]
         public string Username { get; set; } = null!;
 
         [Required]
         public string Password { get; set; } = null!;
+
+        [Required(ErrorMessage = "Please confirm your password.")]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; } = null!;
 
         [Required]
         public string FullName { get; set; } = null!;
@@ -23,7 +25,6 @@ namespace AdvancedProject.Models
         [Required]
         public string? Gender { get; set; }
 
-        // Tenant fields
         [Required]
         public DateTime Dob { get; set; } = DateTime.Today;
 
