ddd4d2ca048f712c25bbfa1f7de854c6e57949c1
RaghadAlesakfi <202302130@student.polytechnic.bh>
2026-05-14 20:44:57 +0300

    fixed maintince request ui again :)

--- AdvancedProject/AdvancedProject/obj/Debug/net9.0/AdvancedProject.AssemblyInfo.cs
+++ AdvancedProject/AdvancedProject/obj/Debug/net9.0/AdvancedProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdvancedProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b11b88d64cc596f8aa7d099c3ec07b14c64adcb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4555bf093047c6e32fd1b6af249eb317f0de2e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdvancedProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdvancedProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
