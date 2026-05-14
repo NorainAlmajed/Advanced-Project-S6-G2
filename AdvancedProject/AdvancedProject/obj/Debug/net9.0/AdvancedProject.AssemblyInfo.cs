d330c8d473e6024ae59bdca7d27379b0712719a7
RaghadAlesakfi <202302130@student.polytechnic.bh>
2026-05-15 02:20:38 +0300

    fix the UI for create Tenant , create property, edit property

--- AdvancedProject/AdvancedProject/obj/Debug/net9.0/AdvancedProject.AssemblyInfo.cs
+++ AdvancedProject/AdvancedProject/obj/Debug/net9.0/AdvancedProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdvancedProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f63f7312e75bf7a0b9090d816ec130a0bdbc53ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+084101cd9ff4a65231fc9a6c59b06b863292fd28")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdvancedProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdvancedProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
