3fbfbd403ab179020b8ac0992e20bac5901ffb4e
Zahraa Hubail <202305220@student.polytechnic.bh>
2026-05-15 02:31:22 +0300

    Before working on API

--- AdvancedProject/AdvancedProject/obj/Debug/net9.0/AdvancedProject.AssemblyInfo.cs
+++ AdvancedProject/AdvancedProject/obj/Debug/net9.0/AdvancedProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdvancedProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41640f29eb0f0d6b880c6de7e0730af0e54066d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ea71b668de0466e6a47da65fb398505b4cdb279")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdvancedProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdvancedProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
